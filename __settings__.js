aeefb5143a4d944ca59876b7ba41ef1941ff6ce6
ShivamKR12 <sonya.shree.69@gmail.com>
2026-03-08 14:32:04 +0530

    Initial Commit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2172694.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 214487150, 214487155, 214487162, 214487151, 214487177 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/214487172/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/214487359/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/214487153/1/ammo.js', 'preload' : true},
+];
